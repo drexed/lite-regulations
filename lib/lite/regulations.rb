e23478140b7262dc467a0e56349e96d647c37e52
Juan Gomez <j.gomez@drexed.com>
2021-07-21 21:49:30 -0400

    Update railtie

--- lib/lite/regulations.rb
+++ lib/lite/regulations.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
-require 'active_record'
-require 'active_support'
+require 'active_record' unless defined?(ActiveRecord)
+require 'active_support' unless defined?(ActiveSupport)
 
+require 'lite/regulations/railtie' if defined?(Rails::Railtie)
 require 'lite/regulations/version'
-require 'lite/regulations/railtie' if defined?(Rails)
 
 %w[base activation containment expiration quarantine suspension visibility].each do |name|
   require "lite/regulations/#{name}"
